8e405837dc6f0cd684c1d5d282ee6290a906eb42
Shawn Bataju <bshawngreen@gmail.com>
2022-12-20 22:26:32 -0600

    Week 13 Code

---
+++ src/main/java/com/promineotech/jeep/JeepSales.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.promineotech.jeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeepSales {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(JeepSales.class, args);
+
+  }
+
+}
+ 
